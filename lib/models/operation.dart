240d25c9c207aef2e51231bc4b22748b5fe5ea13
Giuseppe Gambino <giuseppe.gambino@vigilfuoco.it>
2019-12-12 09:56:45 +0100

    UPDATE Add toMap and fromMap

--- lib/models/operation.dart
+++ lib/models/operation.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:stapp_ri/helpers/db_values.dart';
 
 import 'media.dart';
 
 class Operation {
+  int _id;
   String title;
   String description;
   DateTime date;
@@ -21,4 +23,32 @@ class Operation {
       this.videos,
       this.coordinates,
       @required this.status});
+
+  get id{ return this._id; }
+
+  Operation.fromMap(Map<String, dynamic> map) {
+    this._id = map[DBValues.opId];
+    this.title = map[DBValues.opTitle];
+    this.description = map[DBValues.opDescription];
+    this.date = DateTime.parse(map[DBValues.opDate]);
+    this.coordinates = map[DBValues.opCoordinates];
+    this.status = map[DBValues.opStatus];
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      DBValues.opTitle: this.title,
+      DBValues.opDescription: this.description,
+      DBValues.opDate: this.date.toIso8601String(),
+      DBValues.opCoordinates: this.coordinates,
+      DBValues.opStatus: this.status,
+    };
+    if (_id != null) {
+      map[DBValues.opId] = _id;
+    }
+    if (coordinates != null) {
+      map[DBValues.opCoordinates] = this.coordinates;
+    }
+    return map;
+  }
 }
