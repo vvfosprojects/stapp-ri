3c39ce9584ac4462e1175019d97ca46afcedc743
Giuseppe Gambino <giuseppe.gambino@vigilfuoco.it>
2019-12-18 16:50:13 +0100

    UPDATE model rename

--- lib/models/emergency_operation.dart
+++ lib/models/emergency_operation.dart
@@ -3,30 +3,26 @@ import 'package:stapp_ri/helpers/db_values.dart';
 
 import 'media.dart';
 
-class Operation {
+class EmergencyOperation {
   int _id;
   String title;
   String description;
   DateTime date;
-  List<Media> pictures;
-  List<Media> videos;
-  List<Media> audios;
+  List<Media> media;
   String coordinates;
   String status;
 
-  Operation(
+  EmergencyOperation(
       {@required this.title,
       @required this.description,
       @required this.date,
-      this.pictures,
-      this.audios,
-      this.videos,
+      this.media,
       this.coordinates,
       @required this.status});
 
   get id{ return this._id; }
 
-  Operation.fromMap(Map<String, dynamic> map) {
+  EmergencyOperation.fromMap(Map<String, dynamic> map) {
     this._id = map[DBValues.opId];
     this.title = map[DBValues.opTitle];
     this.description = map[DBValues.opDescription];
