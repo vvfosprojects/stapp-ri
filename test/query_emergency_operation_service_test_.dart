f81088041400e21d07c2868d2926b56b54bc2fae
Giuseppe Gambino <giuseppe.gambino@vigilfuoco.it>
2019-12-18 16:50:44 +0100

    UPDATE text disabled

---
+++ test/query_emergency_operation_service_test_.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stapp_ri/adapters/query_operation_service_adapter.dart';
+import 'package:stapp_ri/models/emergency_operation.dart';
+
+
+void main(){
+  test("Query Emergency Operation", () async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    QueryOperationServiceAdapter adapter = QueryOperationServiceAdapter();
+    List<EmergencyOperation> result = await adapter.readAll();
+    assert(result[0].media.length>0);
+  }
+  
+  );
+}
